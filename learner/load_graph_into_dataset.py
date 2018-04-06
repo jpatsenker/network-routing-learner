afb6468f3b00b6138b4ea714515c899f1f6401da
Jon Patsenker <jonathan@patsenker.com>
2018-04-06 02:19:49 -0400

    stuff wrok

--- learner/load_graph_into_dataset.py
+++ learner/load_graph_into_dataset.py
@@ -9,7 +9,7 @@ import cProfile as p
 from multiprocessing import Process
 from multiprocessing import Manager
 sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
-os.system("taskset -c 0-10 -p %d" % os.getpid())
+os.system("taskset -p %d -c 0-40" % os.getpid())
 
 from core.user import User
 
