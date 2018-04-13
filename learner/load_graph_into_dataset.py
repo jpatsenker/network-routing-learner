330685c8421aa573d300caaaa23bdeb0f81be16a
Jon Patsenker <jonathan@patsenker.com>
2018-04-13 00:41:40 -0400

    stuff wrok

--- learner/load_graph_into_dataset.py
+++ learner/load_graph_into_dataset.py
@@ -1,4 +1,4 @@
-import cPickle as pickle
+import pickle as pickle
 from copy import copy
 import os
 import sys
@@ -6,6 +6,7 @@ import math
 import numpy as np
 import random
 import cProfile as p
+import pandas as pd
 from multiprocessing import Process
 from multiprocessing import Manager
 sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
@@ -16,14 +17,14 @@ from core.user import User
 def reindex_dict(nodes):
 	inodes = dict()
 	ndic = dict(zip(range(len(nodes.keys())), nodes.keys()))
-	rev = {v: k for k, v in ndic.iteritems()}
+	rev = {v: k for k, v in ndic.items()}
 	for n in range(len(nodes)):
 		inodes[n] = copy(nodes[ndic[n]])
 		inodes[n].uid = n
 	#   if n % 1000 == 0:
 	#       print "Done: ", n
 	for n in inodes:
-		inodes[n].friends = map(lambda f: rev[f], inodes[n].friends)
+		inodes[n].friends = list(map(lambda f: rev[f], inodes[n].friends))
 	#   if n % 1000 == 0:
 	#   	print "Done: ", n
 	return inodes
@@ -54,7 +55,7 @@ def distance(p0, p1, rad = EARTH_RAD_KM):
 	return rad*d_sigma
 
 def load_dict():
-	nodes = pickle.load(open("GraphSets/test_graph.pkl", 'r'))
+	nodes = pickle.load(open('GraphSets/test_graph.pkl','rb'))
 	return nodes
 
 
@@ -78,46 +79,104 @@ def load_dict():
 # 					count+=1
 
 
-def graph_to_dataset_file(graph,filename,shps="shortest_paths.txt",exps="expected_paths.txt"):
+def graph_to_dataset_file(graph,filename,shps,exps):
 	with open(filename,'w') as writer:
 		#with open(shps) as shpsr:
-		with open(exps) as expsr:
-			for destination in graph:
-				#print 'hi', time.time()-t
-				#target1_line=shpsr.readline()
-				target2_line=expsr.readline()
-				t1line = np.array(bfs(graph,destination))
-				t2line = np.fromstring(target2_line, float, sep=' ')
-				d=graph[destination]
-				for source in graph:
-					s=graph[source]
-					sf = s.friends
-					temp1=t1line[sf]
-					temp2=t2line[sf]
-					if len(sf)>0:
-						t1 = np.argmin(temp1)
-						t2 = np.argmin(temp2)
+		for destination in graph:
+			print(destination,time.time())
+			#target1_line=shpsr.readline()
+			t1line = shps[destination]
+			t2line = exps[destination]
+			d=graph[destination]
+			for source in graph:
+				s=graph[source]
+				sf = s.friends
+				temp1=t1line[sf]
+				temp2=t2line[sf]
+				if len(sf)>0:
+					t1 = np.argmin(temp1)
+					t2 = np.argmin(temp2)
+				t1n=0.
+				t2n=0.
+				count = 0
+				for neighbor in sf:
 					t1n=0.
 					t2n=0.
-					count = 0
-					for neighbor in sf:
-						t1n=0.
-						t2n=0.
-						if temp1[count] == t1:
-							t1n=1.
-						if temp2[count] == t2:
-							t2n=1.
-						n = graph[neighbor]
-						deg = n.deg1
-						wdeg = math.log(deg)
-						d = graph[destination]
-						dist = distance(n.pos,d.pos)
-						wdist = sig1(dist)
-						cic = len(set(n.comm).intersection(d.comm))
-						#medpower = np.median(map(lambda z: graph[z].deg1, n.friends))
-						#locality = sum(map(lambda z: distance(graph[z].pos,s.pos), n.friends))
-						writer.write("\t".join([str(dist),str(wdist),str(cic),str(deg),str(wdeg),str(t1n),str(t2n)]) + '\n')
-						count+=1
+					if temp1[count] == t1:
+						t1n=1.
+					if temp2[count] == t2:
+						t2n=1.
+					n = graph[neighbor]
+					deg = n.deg1
+					wdeg = math.log(deg)
+					d = graph[destination]
+					dist = distance(n.pos,d.pos)
+					wdist = sig1(dist)
+					cic = len(set(n.comm).intersection(d.comm))
+					#print(neighbor,n.friends)
+					medpower = np.median(list(map(lambda z: graph[z].deg1, n.friends)))
+					locality = sum(map(lambda z: distance(graph[z].pos,s.pos), n.friends))
+					writer.write("\t".join([str(dist),str(wdist),str(cic),str(deg),str(wdeg),str(medpower),str(locality),str(t1n),str(t2n)]) + '\n')
+					count+=1
+
+def dataset_delegate(graph,shps,exps,r,writer):
+	sc=0
+	for destination in range(r[0],r[1]):
+		#print(destination)
+		t1line = shps[destination-r[0]]
+		t2line = exps[destination-r[0]]
+		d=graph[destination]
+		for source in graph:
+			s=graph[source]
+			sf = s.friends
+			temp1=t1line[sf]
+			temp2=t2line[sf]
+			if len(sf)>0:
+				t1 = np.argmin(temp1)
+				t2 = np.argmin(temp2)
+			count = 0
+			for neighbor in sf:
+				t1n=0.
+				t2n=0.
+				if temp1[count] == t1:
+					t1n=1.
+				if temp2[count] == t2:
+					t2n=1.
+				n = graph[neighbor]
+				deg = n.deg1
+				wdeg = math.log(deg)
+				d = graph[destination]
+				dist = distance(n.pos,d.pos)
+				wdist = sig1(dist)
+				cic = len(set(n.comm).intersection(d.comm))
+				medpower = np.median(list(map(lambda z: graph[z].deg1, n.friends)))
+				locality = sum(map(lambda z: distance(graph[z].pos,s.pos), n.friends))
+				writer.write("\t".join([str(dist),str(wdist),str(cic),str(deg),str(wdeg),str(medpower),str(locality),str(t1n),str(t2n)]) + '\n')
+				count+=1
+				sc+=1
+	writer.close()
+
+def graph_to_dataset_file_multi(graph,filename,shps,exps,cores):
+	writers = [open(filename+str(q)+".txt",'w') for q in range(cores)]
+	ps = []
+	l=len(graph)
+	divs = list(range(0,l,int(math.ceil(float(l)/float(cores)))))
+	divs.append(len(graph))
+	for w in range(len(writers)):
+		ps.append(Process(target=dataset_delegate, args=(graph,shps,exps,(divs[w],divs[w+1]),writers[w])))
+		ps[-1].start()
+	for p in range(len(ps)):
+		ps[p].join()
+	finwrite=open(filename + '.txt','w')
+	readers = [open(filename+str(q)+".txt",'r') for q in range(len(writers))]
+	for r in readers:
+		s=r.read()
+		finwrite.write(s)
+		r.close()
+	finwrite.close()
+
+
+
 
 def get_avg_and_var(dataset_file):
 	avg = 0
@@ -138,14 +197,24 @@ def get_avg_and_var(dataset_file):
 
 def normalize_dataset(dataset_file, out_file, cols):
 	avg, var = get_avg_and_var(dataset_file)
-	print "hi"
 	with open(dataset_file) as reader:
 		with open(out_file, "w") as writer:
 			line = reader.readline()
 			while line:
 				x = np.fromstring(line, dtype='float', sep=' ')
 				normal = (x[cols]-avg[cols])/(var[cols]**2)
-				writer.write(" ".join(map(str, normal)))
+				writer.write(" ".join(list(map(str, normal))))
+				line=reader.readline()
+
+def normalize_dataset_multi(dataset_file, out_file, cols, cores):
+	avg, var = get_avg_and_var(dataset_file, cores)
+	with open(dataset_file) as reader:
+		with open(out_file, "w") as writer:
+			line = reader.readline()
+			while line:
+				x = np.fromstring(line, dtype='float', sep=' ')
+				normal = (x[cols]-avg[cols])/(var[cols]**2)
+				writer.write(" ".join(list(map(str, normal))))
 				line=reader.readline()
 
 def clear_comms(graph):
@@ -255,7 +324,7 @@ def calculate_shortest_paths_to_file_multi(graph, filename, cores):
 	fnames = [filename+str(q+10)+".txt" for q in range(cores)]
 	writers = [open(filename+str(q+10)+".txt",'w') for q in range(cores)]
 	l = len(graph)
-	divs = range(0,l,int(math.ceil(float(l)/float(cores))))
+	divs = list(range(0,l,int(math.ceil(float(l)/float(cores)))))
 	divs.append(len(graph))
 	ps = []
 	for w in range(len(writers)):
@@ -278,9 +347,6 @@ def calculate_shortest_paths_to_file_multi(graph, filename, cores):
 	#os.system('ls '+filename+'* | sort | while read fn ; do cat "$fn" >> '+filename+'.txt; done')
 	#os.system("cat "+filename+"* > "+filename+".txt")
 
-#TODO
-def exps_delegate():
-	pass
 
 # def nav_readers_to_divs(reader,divs):
 # 	c=1
@@ -290,17 +356,19 @@ def exps_delegate():
 # 		lines = map(lambda r: r.readline(), reader)
 # 	map(lambda r: r.close(), reader)
 
-def exps_delegate(graph,prev_paths,next_paths,r):
+def exps_delegate(graph,prev_paths,next_paths,r,q):
+	#os.system("taskset -p -c " + str(q) + " " + str(os.getpid()))
 	for i in range(r[0],r[1]):
 		for s in graph:
-			sfriends = graph[s].friends
-			if s==d:
+			sfriends = list(graph[s].friends)
+			if s==i:
 				next_paths[i,s] = 0.
 				continue
-			if d in sfriends:
+			if i in sfriends:
 				next_paths[i,s] = 1.
 				continue
-			next_paths[i,s] = sum(map(lambda f: (prev_paths[f]+1)*1./len(sfriends),sfriends))
+			l = len(sfriends)
+			next_paths[i,s] = sum(map(lambda f: (prev_paths[i,f]+1)*1./l,sfriends))
 
 
 def calculate_expected_paths_mult(graph, filename,prev_paths,cores):
@@ -308,18 +376,22 @@ def calculate_expected_paths_mult(graph, filename,prev_paths,cores):
 	# writer = [open(filename+str(q+10)+".txt",'w') for q in range(cores)]
 	# reader = [open(filename+str(q+10)+".txt",'r') for q in range(cores)]
 	l = len(graph)
-	divs = range(0,l,int(math.ceil(float(l)/float(cores))))
+	divs = list(range(0,l,int(math.ceil(float(l)/float(cores)))))
 	divs.append(l)
 	#nav_readers_to_divs(reader, divs)
 	c=0
 	while c < 10:
 		ps = []
+		q=0
 		for d in range(len(divs)-1):
 			next_paths = np.copy(prev_paths)
-			ps.append(Process(target = exps_delegate, args = (graph,prev_paths,next_paths,(divs[d],divs[d+1]))))
+			ps.append(Process(target = exps_delegate, args = (graph,prev_paths,next_paths,(divs[d],divs[d+1]),q)))
+			ps[-1].start()
+			q+=1
 		for p in range(len(ps)):
-			p.join()
+			ps[p].join()
 		c+=1
+		print("Iter", c, "complete")
 	return next_paths
 
 def calculate_expected_paths_to_file(graph,filename,paths_file):
@@ -351,10 +423,13 @@ def test_graph(n):
 			if i in g[j].friends:
 				friends.append(j)
 		for j in range(i+1,n):
-			if random.random()>0.5:
+			if random.random()<1./float(n):
 				friends.append(j)
 		g[i]=User(None,[1,2],(random.random(),random.random()),friends)
 	return g
+
+
+
 
 
 import time
@@ -363,20 +438,33 @@ t=time.time()
 d = load_dict()
 
 
-#d = test_graph(100)
+#d = test_graph(1000)
 
 d = switch_communities(d, "commfile.txt")
 
 
 
-print "LOADED: ", time.time()-t
+print ("LOADED: ", time.time()-t)
 d=reindex_dict(d)
-print "INDEXED: ", time.time()-t
+print ("INDEXED: ", time.time()-t)
 #calculate_shortest_paths_to_file_multi(d, "temp/shortest_paths",50)
-print "SHPS: ", time.time()-t
-calculate_expected_paths_to_file(d, "temp/expected_paths.txt", "temp/shortest_paths.txt")
-print "EXPS: ", time.time()-t
-p.run('graph_to_dataset_file(d,"gowalla_ml_dataset.txt")')
-print "TOFILE: ", time.time()-t
-normalize_dataset("gowalla_ml_dataset.txt", "gowalla_ml_dataset_norm.txt", range(5))
-print "NORMALIZED: ", time.time()-t
+#shps = pd.io.parsers.read_csv("temp/shortest_paths.txt",sep=' ',header=None,engine='c',dtype='float32').as_matrix()
+shps_str = open("temp/shortest_paths.txt").read()
+print ("READ SHPS: ", time.time()-t)
+shps_arr = shps_str.split("\n")
+print ("SPLIT ROWS: ", time.time()-t)
+shps_darr = map(lambda x: x.split(' '), shps_arr)
+shps_darr=list(shps_darr)
+print ("SPLIT COLS: ", time.time()-t)
+shps = np.array(shps_darr[:-1])
+print ("AS NUMPY: ", time.time()-t)
+del shps_str, shps_arr, shps_darr
+print ("DELETE: ", time.time()-t)
+shps = shps.astype('int')
+print ("SHPS: ", time.time()-t)
+exps=calculate_expected_paths_mult(d, "temp/expected_paths.txt", shps, 50)
+print ("EXPS: ", time.time()-t)
+graph_to_dataset_file_multi(d,"temp/gowalla_ml_dataset",shps,exps,50)
+print ("TOFILE: ", time.time()-t)
+normalize_dataset("temp/gowalla_ml_dataset.txt", "temp/gowalla_ml_dataset_norm.txt", range(5))
+print ("NORMALIZED: ", time.time()-t)
