3d8eec45777f1fca86ac05f85dd8b0fc6738988a
Jon Patsenker <jonathan@patsenker.com>
2018-04-29 18:35:48 -0400

    bins

--- learner/external_learner.py
+++ learner/external_learner.py
@@ -583,9 +583,9 @@ class ExternalRegressor:
 		return self.final()
 
 	def regressFromFileMultithreadedMultifileBins(self,data,top,bottom,bins=[0,50,100,200,400,800,1600,3200,6400,12800,20000]):
-		self.xtx=np.zeros([50,len(bins),len(top),len(top)])
-		self.xty1=np.zeros([50,len(bins),len(top)])
-		self.xty2=np.zeros([50,len(bins),len(top)])
+		self.xtx=np.zeros([len(bins),len(top),len(top)])
+		self.xty1=np.zeros([len(bins),len(top)])
+		self.xty2=np.zeros([len(bins),len(top)])
 		xtx = np.zeros([50,len(bins),len(top),len(top)])
 		xty1 = np.zeros([50,len(bins),len(top)])
 		xty2 = np.zeros([50,len(bins),len(top)])
