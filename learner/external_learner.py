d04073a3608bd457e0f752031edfc11ee13bfbd5
Jon Patsenker <jonathan@patsenker.com>
2018-04-29 19:42:36 -0400

    bins

--- learner/external_learner.py
+++ learner/external_learner.py
@@ -169,7 +169,7 @@ def delegate_cross_entropy_error_from_multiple_files_bins(w1, w2, f, ret, pnum,t
 			continue
 		line=f.readline()
 		break
-	for b in range(len(bin)-1):
+	for b in range(len(bins)-1):
 		ret[b,pnum]=[se1[b], sg1[b], sh1[b], se2[b], sg2[b], sh2[b]]
 
 def cross_entropy_error_from_file_multithreaded(w1, w2, data, splits,pnum,top,bottom):
