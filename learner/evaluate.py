986c955f5170d6f253d3219d6f398306eb3f427e
Jon Patsenker <jonathan@patsenker.com>
2018-04-30 04:00:42 -0400

    evals

--- learner/evaluate.py
+++ learner/evaluate.py
@@ -76,7 +76,7 @@ def simulation(graph,weights):
 	print("ROUTING FROM ", source, "TO", destination)
 	curr = source
 	hops = 0
-	while curr != destination and hops < 1000:
+	while curr != destination and hops < 100:
 		#print(hops, curr)
 		neighbors = graph[curr].friends
 		if destination in neighbors:
